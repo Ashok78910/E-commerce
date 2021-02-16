8392ea0c11c6c5a38f6b9c5ac7cc3b96501a3b6c
Ashok78910 <ak0671527@gmail.com>
2021-02-16 14:22:34 +0530

    setting home page

---
+++ clothing/src/hompage.component.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './hompage.style.scss';
+export const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Jacket</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Sneakers</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Womens</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Mens</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+);
+
