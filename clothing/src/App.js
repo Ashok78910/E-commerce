98edf23705f10c29626cd20582d53a6bb798f4b1
Ashok78910 <ak0671527@gmail.com>
2021-02-17 14:24:32 +0530

    set up header page

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -3,16 +3,19 @@ import { Switch , Route ,Link} from 'react-router-dom';
 import './App.css';
 import {Homepage} from './pages/homepage/homepage.component';
 import {Shoppage} from './pages/shop/shop.component';
-
+import  {Header} from './component/header-component/header.component';
 function App() {
   return (
     <div >
-   
+    <Header/>
+       <Switch>
      <Route exact path ='/' component={Homepage}/>
      <Route path ='/shop' component={Shoppage}/>
      
-    
+     </Switch>
+     
      </div>
+    
   );
 }
 
